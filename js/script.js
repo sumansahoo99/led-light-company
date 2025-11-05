032ad91150f59542d57de54074a268580d72ba6b
Suman1901 <sumansahoo41243@gmail.com>
2025-11-05 17:19:48 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 6,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 0,
+        disableOnInteraction: true,
+      },
+      speed: 3000,
+      loop: true,
+    //   loopedSlides: 6,
+    });
+
